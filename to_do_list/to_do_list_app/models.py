6a2e43ee007d0499cd26e52236a995df1ffb5848
megahomyak <g.megahomyak@gmail.com>
2022-03-20 21:45:19 +0500

    Adding models!

--- to_do_list/to_do_list_app/models.py
+++ to_do_list/to_do_list_app/models.py
@@ -1,1 +1,13 @@
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class ToDoList(models.Model):
+    title = models.TextField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class ToDoListElement(models.Model):
+    title = models.TextField()
+    is_done = models.BooleanField()
+    to_do_list = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
