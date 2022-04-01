e6588c2bb0778a6500cf886d24afc2c4a392555b
megahomyak <g.megahomyak@gmail.com>
2022-04-01 20:25:35 +0500

    Slightly better string representations
    (not including owner's username, which can be useful, because thus it will create N+1 database queries, which I don't want)

--- to_do_list/to_do_list_app/models.py
+++ to_do_list/to_do_list_app/models.py
@@ -6,8 +6,14 @@ class ToDoList(models.Model):
     title = models.TextField()
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.title
+
 
 class ToDoListItem(models.Model):
     title = models.TextField()
     is_done = models.BooleanField()
     to_do_list = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
